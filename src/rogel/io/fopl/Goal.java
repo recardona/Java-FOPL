a0d59b4415595a9abef69b028380548178e79f9b
Rogelio E. Cardona-Rivera <mail@rogel.io>
2014-11-29 18:01:32 -0500

    Expressions that will appear as goals in an and/or graph must implement this interface.

---
+++ src/rogel/io/fopl/Goal.java
@@ -0,0 +1,9 @@
+package rogel.io.fopl;
+
+/**
+ * Expressions that will appear as goals in an and/or graph must implement this interface.
+ */
+public interface Goal extends PredicateCalculusExpression
+{
+
+}
