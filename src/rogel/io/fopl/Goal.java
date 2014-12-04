e66f7278cb08db6541eb59498a94ede4700ce994
Rogelio E. Cardona-Rivera <mail@rogel.io>
2014-12-03 21:58:08 -0500

    Starting fresh

--- src/rogel/io/fopl/Goal.java
+++
@@ -1,9 +0,0 @@
-package rogel.io.fopl;
-
-/**
- * Expressions that will appear as goals in an and/or graph must implement this interface.
- */
-public interface Goal extends PredicateCalculusExpression
-{
-
-}
