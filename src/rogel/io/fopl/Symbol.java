5da5b39fd3ad0b1fd21516d1cc124d72b03667f8
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-21 19:04:30 -0400

    Simplified the constructor and clarified some of the documentation.

--- src/rogel/io/fopl/Symbol.java
+++ src/rogel/io/fopl/Symbol.java
@@ -1,35 +1,42 @@
 package rogel.io.fopl;
 
+import java.util.Comparator;
 import java.util.HashMap;
 
 /**
- * A Symbol follows the Herbrand interpretation, and is merely a (Java)
- * String that represents itself. There is only <b>one</b> Symbol with
- * any given name (they are memory-unique objects). 
+ * A Symbol follows the Herbrand interpretation, and is merely a (Java) String 
+ * that represents itself. There is only <b>one</b> Symbol with any given name
+ * (they are memory-unique objects). 
  * 
- * @see http://en.wikipedia.org/wiki/Herbrand_interpretation
  * @author recardona
+ * @see http://en.wikipedia.org/wiki/Herbrand_interpretation
  */
-public class Symbol 
-{
-	//In the formal sciences, the domain of discourse is the set of entities over which 
-	//variables of interest in some formal language may range.
-	private static HashMap<String, Symbol> domainOfDiscourse = new HashMap<String, Symbol>(10000);
-	private String name;
+public class Symbol {
 	
+	/**
+	 * The domain of discourse is the set of entities over which variables of
+	 * interest in some formal language may range. 
+	 */
+	private static HashMap<String, Symbol> domainOfDiscourse = new HashMap<String, Symbol>(10000);
+	
+	private static LexicographicSymbolComparator singletonLexicographicSymbolComparator;
+	
+	/**
+	 * The String name this Symbol represents.
+	 */
+	private String name;
 	
 	/**
 	 * Returns a Symbol with the given name. If no such Symbol exists, it creates
 	 * a new one and adds it to the domain of discourse for future retrieval.
-	 * @param name the name of the Symbol that is sought (or the name of the newly
-	 * created Symbol if none with the parameter name exists)
-	 * @return
-	 * @throws IllegalArgumentException
+	 * @param name the name of the Symbol that is sought
+	 * @return a Symbol with the given name
+	 * @throws IllegalArgumentException if the Symbol name is null or empty
 	 */
-	public static Symbol get(String name) throws IllegalArgumentException
-	{
+	public static Symbol get(String name) throws IllegalArgumentException {
+		
 		if((name == null) || (name.equals(""))) {
-			throw new IllegalArgumentException("Attempted to get a Symbol without a name");
+			throw new IllegalArgumentException("Attempted to get a Symbol without a name.");
 		}
 		
 		if(domainOfDiscourse.containsKey(name)) {
@@ -41,33 +48,23 @@ public class Symbol
 		return newSymbol;
 	}
 	
-	
 	/**
-	 * Private constructor
+	 * Constructs a Symbol of the given name.
 	 * @param name the name of the symbol
-	 * @throws IllegalArgumentException if aName is null or empty  
 	 */
-	private Symbol(String name) throws IllegalArgumentException
-	{
-		if((name == null) || (name.equals(""))) {
-			throw new IllegalArgumentException("Symbol names must be non-null, non-empty strings");
-		}
-		
+	private Symbol(String name) {
 		this.name = name;
 	}
-	
 	
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException("Symbol.clone() is not supported");
 	}
 	
-	
 	@Override
 	public String toString() {
 		return this.name;
 	}
-	
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -76,13 +73,36 @@ public class Symbol
 		}
 		
 		Symbol other = (Symbol) obj;
-		return (this.hashCode() == other.hashCode());
+		return (this.name.equals(other.name));
 	}
-	
 	
 	@Override
 	public int hashCode() {
 		return this.name.hashCode();
 	}
 	
+	/**
+	 * Returns a LexicographicSymbolComparator, used to compare two Symbols by
+	 * the Strings they represent.
+	 * @return a comparator of Symbols, which implements the Comparator interface
+	 * @see java.util.Comparator
+	 */
+	public static Comparator<Symbol> getLexicographicSymbolComparator() {
+		//lazy initialization
+		if(singletonLexicographicSymbolComparator == null) {
+			singletonLexicographicSymbolComparator = new LexicographicSymbolComparator();
+		}
+		return singletonLexicographicSymbolComparator;
+	}
+	
+	/**
+	 * A Symbol Comparator which compares the Strings the Symbol represents
+	 * lexicographically.
+	 */
+	private static class LexicographicSymbolComparator implements Comparator<Symbol> {
+		@Override
+		public int compare(Symbol s1, Symbol s2) {
+			return s1.toString().compareTo(s2.toString());
+		}
+	}	
 }
