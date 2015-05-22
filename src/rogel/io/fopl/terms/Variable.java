f6977978ff15b08d26e416edd9bcd8758d812259
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-22 11:31:53 -0400

    Committing initial version of the Variable class implementation of the unify method.

--- src/rogel/io/fopl/terms/Variable.java
+++ src/rogel/io/fopl/terms/Variable.java
@@ -1,6 +1,8 @@
 package rogel.io.fopl.terms;
 
+import rogel.io.fopl.Substitution;
 import rogel.io.fopl.Symbol;
+import rogel.io.fopl.Unifiable;
 
 /**
  * A Variable is a "placeholder" Term, which can be assigned values through a
@@ -30,6 +32,29 @@ public class Variable extends Term {
 		super(symbol);
 	}
 
+	@Override
+	public Substitution unify(Unifiable unifiable, Substitution substitution) {
+		
+		if(this == unifiable) {
+			// If they're the same, then the substitution cannot change.
+			return substitution;
+		}
+		
+		if(substitution.isBound(this)) {
+			// If this Variable is bound within the parameter Substitution,
+			// the unification must continue with the bound value.
+			return substitution.getBinding(this).unify(unifiable, substitution);
+		}
+		
+		// At this point, we're attempting to add a new binding. First,
+		// initialize a new Substitution with the parameter Substitution.
+		Substitution sigma = new Substitution(substitution);
+		
+		// Add the new binding and return it.
+		sigma.add(this, unifiable);
+		return sigma;
+	}
+	
 	@Override
 	public String toString() {
 		return "[Variable: "+this.symbol.toString()+"]";
