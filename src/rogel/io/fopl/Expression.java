42883d8294f9dc5b0776ca9759ae8fd613be9cfc
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-22 14:10:44 -0400

    Adding the Expression interface, which all FOPL expressions should implement.

---
+++ src/rogel/io/fopl/Expression.java
@@ -0,0 +1,17 @@
+package rogel.io.fopl;
+
+/**
+ * An Expression represents anything that can be expressed in FOPL.	
+ * @author recardona
+ */
+public interface Expression {
+
+	/**
+	 * Replaces any bound Variable in this Expression with its binding in the
+	 * Substitution, if it exists.
+	 * @param substitution the Substitution set of Variables
+	 * @return an Expression with substituted Variables.
+	 */
+	public Expression replaceVariables(Substitution substitution);
+	
+}
