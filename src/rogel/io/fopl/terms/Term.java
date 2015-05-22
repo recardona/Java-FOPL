934fab66920f34835a2399d8c56f3dd63fb2096e
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-22 01:44:03 -0400

    Moved the static method to the bottom of the class.

--- src/rogel/io/fopl/terms/Term.java
+++ src/rogel/io/fopl/terms/Term.java
@@ -18,6 +18,63 @@ public abstract class Term {
 	protected Symbol symbol;
 	
 	/**
+	 * Defines a Term with the given name. If the name is a String that did not
+	 * already exist within the domain of discourse (defined as a Symbol), then
+	 * a new Symbol is created.
+	 * @param name the name of this Term
+	 */
+	protected Term(String name) {
+		this.symbol = Symbol.get(name);
+	}
+	
+	/**
+	 * Defines a Term with the given Symbol.
+	 * @param symbol the Symbol that represents this Term within the domain of
+	 * discourse
+	 */
+	protected Term(Symbol symbol) {
+		this.symbol = symbol;
+	}
+	
+	/**
+	 * @return the symbol
+	 */
+	public Symbol getSymbol() {
+		return this.symbol;
+	}
+	
+	@Override
+	public String toString() {
+		if(this instanceof Function) {
+			return ((Function) this).toString();
+		}
+		
+		if(this instanceof Variable) {
+			return ((Variable) this).toString();
+		}
+		
+		else {
+			return "[Term: " + this.symbol.toString() + "]";
+		}
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		//Term equality is composed of type and symbol equality
+		if(!(obj instanceof Term)) {
+			return false;
+		}
+		
+		Term other = (Term) obj;
+		return this.symbol.equals(other.symbol);		
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.symbol.hashCode();
+	}
+	
+	/**
 	 * Generates a two-dimensional array of Terms by flattening the parameter
 	 * Set of Terms. Each row represents a different Term, with their 
 	 * associated columns containing all the Symbols that Term contains.
@@ -90,63 +147,6 @@ public abstract class Term {
 		}
 		
 		return flattenedTerms;
-	}
-	
-	/**
-	 * Defines a Term with the given name. If the name is a String that did not
-	 * already exist within the domain of discourse (defined as a Symbol), then
-	 * a new Symbol is created.
-	 * @param name the name of this Term
-	 */
-	protected Term(String name) {
-		this.symbol = Symbol.get(name);
-	}
-	
-	/**
-	 * Defines a Term with the given Symbol.
-	 * @param symbol the Symbol that represents this Term within the domain of
-	 * discourse
-	 */
-	protected Term(Symbol symbol) {
-		this.symbol = symbol;
-	}
-	
-	/**
-	 * @return the symbol
-	 */
-	public Symbol getSymbol() {
-		return this.symbol;
-	}
-	
-	@Override
-	public String toString() {
-		if(this instanceof Function) {
-			return ((Function) this).toString();
-		}
-		
-		if(this instanceof Variable) {
-			return ((Variable) this).toString();
-		}
-		
-		else {
-			return "[Term: " + this.symbol.toString() + "]";
-		}
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		//Term equality is composed of type and symbol equality
-		if(!(obj instanceof Term)) {
-			return false;
-		}
-		
-		Term other = (Term) obj;
-		return this.symbol.equals(other.symbol);		
-	}
-	
-	@Override
-	public int hashCode() {
-		return this.symbol.hashCode();
 	}
 	
 	// The Comparator<Term> singleton.
