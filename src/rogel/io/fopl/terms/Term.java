bdc1aaa99344d42482d7e8b0f605d8ea8bcbaa70
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-22 14:00:42 -0400

    Removed the TermTest, since the method it tested no longer exists.

--- src/rogel/io/fopl/terms/Term.java
+++ src/rogel/io/fopl/terms/Term.java
@@ -1,8 +1,6 @@
 package rogel.io.fopl.terms;
 
 import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 
 import rogel.io.fopl.Symbol;
 import rogel.io.fopl.Unifiable;
@@ -73,81 +71,6 @@ public abstract class Term implements Unifiable {
 	@Override
 	public int hashCode() {
 		return this.symbol.hashCode();
-	}
-	
-	/**
-	 * Generates a two-dimensional array of Terms by flattening the parameter
-	 * Set of Terms. Each row represents a different Term, with their 
-	 * associated columns containing all the Symbols that Term contains.
-	 * <p> 
-	 * For instance, for the Set of Terms <code>{f(x), y}</code> (a Function
-	 * and a Variable, respectively), the following two-dimensional array 
-	 * represents the flattened version: 
-	 * <code>[ [f(x), x] [y, <b>null</b>] ]</code>. 
-	 *  
-	 * @param terms the Set of Terms to flatten
-	 * @return a two-dimensional array of Terms that represents the parameter 
-	 * terms in flattened form
-	 */
-	public static Term[][] flatten(Set<Term> terms) {
-		
-		// Find the Term within the Set with the greatest number of Symbols.
-		// The lowest it could be is 1 (instead of 0) because a Term must be a 
-		// Function or Variable, and Variables always have a Symbol count of 1.
-		int maxSymbolCount = 1;
-		for(Term t : terms) {
-			
-			// Only check Functions, because Variables
-			// cannot be of greater Symbol count.
-			if(t instanceof Function) {
-				
-				Function functionTerm = (Function) t;
-				
-				// The number of Symbols for a function is equal to 1 (the 
-				// Function Symbol itself) + the Symbols for each of the 
-				// Function's arguments (given by the Function's arity).
-				int symbolCount = 1 + functionTerm.getArity();
-
-				// Update the max if we found a higher number of Symbols.
-				if(symbolCount > maxSymbolCount) {
-					maxSymbolCount = symbolCount;
-				}
-			}
-		}
-		
-		// Get the parameter terms as an array.
-		Term[] parameterTerms = terms.toArray(new Term[terms.size()]);
-		
-		// Prepare the flattenedTerms two-dimensional array.
-		Term[][] flattenedTerms = new Term[terms.size()][maxSymbolCount];
-		for(int termIndex = 0; termIndex < terms.size(); termIndex++) {
-			
-			// The parameter Term goes in the first column.
-			Term t = parameterTerms[termIndex];
-			flattenedTerms[termIndex][0] = t; 
-			
-			// If the Term is a non-constant Function, however, we 
-			// need to put all its Terms into the other columns.
-			if(t instanceof Function) {
-				
-				Function functionTerm = (Function) t;
-				if(! functionTerm.isConstant()) {
-					
-					// Iterate through the Function argument terms, and place them
-					// in the flattenedTerms.
-					List<Term> functionArguments = functionTerm.getArguments();
-					
-					for(int argIndex = 0; argIndex < functionArguments.size(); argIndex++) {
-						
-						// Can't start at functionArgumentIndex, because
-						// index 0 is the Function Term itself.
-						flattenedTerms[termIndex][argIndex+1] = functionArguments.get(argIndex); 
-					}
-				}
-			}
-		}
-		
-		return flattenedTerms;
 	}
 	
 	// The Comparator<Term> singleton.
