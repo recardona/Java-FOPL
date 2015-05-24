06a5ed684521c57725e8329bf1f78d46b1d13145
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-05-24 17:33:19 -0400

    Added documentation for the NegatedFormula.

--- src/rogel/io/fopl/formulas/NegatedFormula.java
+++ src/rogel/io/fopl/formulas/NegatedFormula.java
@@ -12,16 +12,37 @@ public class NegatedFormula extends Formula {
 	/** The Formula this class negates. */
 	private Formula formula;
 	
+	/**
+	 * Constructs a NegatedFormula with the given name, over the given Formula.
+	 * If the name is a String that did not already exist within the domain of
+	 * discourse (i.e. was already defined as a Symbol), then a new Symbol is
+	 * created and added to the domain of discourse. The value of this Formula
+	 * is the opposite of the parameter Formula (the Formula it describes). 
+	 * @param name the name of the Formula
+	 * @param formula the Formula this NegatedFormula describes
+	 */
 	public NegatedFormula(String name, Formula formula) {
 		super(name);
 		this.formula = formula; // The Formula this class describes.
 		this.value = !this.formula.value; // This class' value is the opposite of the Formula it describes.
 	}
 	
+	/**
+	 * Constructs a NegatedFormula with the given Symbol, over the given
+	 * Formula. The value of this Formula is the opposite of the parameter
+	 * Formula (the Formula it describes).
+	 * @param symbol the Symbol that represents this NegatedFormula within the
+	 *   domain of discourse
+	 * @param formula the Formula this NegatedFormula describes
+	 */
 	public NegatedFormula(Symbol symbol, Formula formula) {
 		super(symbol);
 		this.formula = formula;
 		this.value = !this.formula.value;
 	}
 	
+	@Override
+	public String toString() {
+		return "(not " + formula + ")";
+	}
 }
