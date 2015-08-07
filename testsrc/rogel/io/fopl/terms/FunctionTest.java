99680c4e5612db160de0ef2a636bf88157d9a574
Rogelio E. Cardona-Rivera <recardon@ncsu.edu>
2015-08-07 13:55:35 -0400

    Removing unneeded imports.

--- testsrc/rogel/io/fopl/terms/FunctionTest.java
+++ testsrc/rogel/io/fopl/terms/FunctionTest.java
@@ -3,8 +3,6 @@ package rogel.io.fopl.terms;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
