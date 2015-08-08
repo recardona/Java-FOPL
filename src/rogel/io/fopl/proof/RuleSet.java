a691019f5099bd3b2762f53d5dcc6b81583d9196
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-08-08 17:09:57 -0400

    Initial implementation of the RuleSet class, which is the logical basis for theorem proving.

---
+++ src/rogel/io/fopl/proof/RuleSet.java
@@ -0,0 +1,55 @@
+package rogel.io.fopl.proof;
+
+import java.util.HashMap;
+
+import rogel.io.fopl.terms.Variable;
+import rogel.io.util.VarargsUtils;
+
+/**
+ * The RuleSet is the logic base, a list of HornClauses that are used during the construction of
+ * the proof tree.
+ * @author recardona
+ */
+public class RuleSet {
+	
+	/** The HornClauses that define the logic basis for theorem proving. */
+	private HornClause[] rules;
+	
+	/**
+	 * Constructs a RuleSet from a list of HornClauses.
+	 * @param rules a varargs of HornClause objects that collectively define the logic base.
+	 */
+	public RuleSet(HornClause... rules) {
+		VarargsUtils.throwExceptionOnNull((Object[]) rules);
+		this.rules = rules;
+	}
+	
+	/**
+	 * Returns the HornClause rule at the given index within the RuleSet.
+	 * @param index the index of the rule within the RuleSet to get.
+	 * @return a HornClause.
+	 */
+	public HornClause getRule(int index) {
+		return this.rules[index];
+	}
+	
+	/**
+	 * Returns the HornClause rule at the given index within the RuleSet with its Variables 
+	 * standardized apart.
+	 * @param index the index of the rule within the RuleSet to get.
+	 * @return a HornClause with Variables that have been standardized apart.
+	 */
+	public HornClause getRuleStandardizedApart(int index) {		
+		HornClause rule = (HornClause) this.rules[index];
+		HashMap<Variable, Variable> standardizedVariables = new HashMap<Variable, Variable>();
+		HornClause standardizedApartRule = (HornClause) rule.standardizeVariablesApart(standardizedVariables);
+		return standardizedApartRule;
+	}
+	
+	/**
+	 * @return the number of HornClause rules within this RuleSet.
+	 */
+	public int getRuleCount() {
+		return rules.length;
+	}
+}
