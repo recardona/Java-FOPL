721b9a11cb563baa14bc896f18bba2d6700bd10b
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-08-08 22:42:36 -0400

    Added documentation to clarify what I am doing.

--- src/rogel/io/fopl/proof/tree/package-info.java
+++ src/rogel/io/fopl/proof/tree/package-info.java
@@ -1,6 +1,7 @@
 /**
- * These classes form the foundation of the logic-based reasoner, which constructs the search space
- * defined by sequences of valid inferences on a set of FOPL sentences.
+ * The classes in this package form the foundation of the logic-based reasoner, which solves the 
+ * logical satisfiability problem: given a set of facts about some domain, and a query over that
+ * domain, does there exist a set of Substitutions that satisfy the query? 
  * @author recardona
  */
 package rogel.io.fopl.proof.tree;
