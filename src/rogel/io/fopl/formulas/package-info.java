76884e843450f2c85c72829c94680803b6e95b7e
Rogelio E. Cardona-Rivera <mail@rogel.io>
2015-08-11 20:12:04 -0400

    Added newline.

--- src/rogel/io/fopl/formulas/package-info.java
+++ src/rogel/io/fopl/formulas/package-info.java
@@ -2,6 +2,7 @@
  * Classes related to Formulas within FOPL. Formulas state conditions, which can be true or false.
  * Formulas require at least one Symbol to designate what kind of condition they identify as true 
  * or false.
+ * 
  * @author recardona
  */
 package rogel.io.fopl.formulas;
